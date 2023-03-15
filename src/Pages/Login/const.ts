512d3940eb8626606a1102b7a7676a941702ba93
ingaSiu <ingasiud@gmail.com>
2023-03-15 20:39:56 +0200

    validation with yup

--- src/Pages/Login/const.ts
+++ src/Pages/Login/const.ts
@@ -1,6 +1,15 @@
+import * as Yup from 'yup';
+
+import { emailValidation, requiredField } from '../../consts/validation';
+
 import { User } from '../../Types/user';
 
 export const loginFormInitialValues: User = {
   email: '',
   password: '',
 };
+
+export const loginValidationSchema: Yup.ObjectSchema<User> = Yup.object().shape({
+  email: Yup.string().email().required(requiredField),
+  password: Yup.string().required(emailValidation),
+});
