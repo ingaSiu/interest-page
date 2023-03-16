7f888faa2c59cc0aa6f5db5de4226d6d2680d151
ingaSiu <ingasiud@gmail.com>
2023-03-16 19:58:02 +0200

    added argument to fetchImg get

--- src/api/images.ts
+++ src/api/images.ts
@@ -4,6 +4,6 @@ import axios from 'axios';
 const URL = 'https://pixabay.com/api';
 const KEY = '34412175-0885be20bb5588c5c008fe206';
 
-export const fetchImages = (): Promise<PixabayImage[]> => {
-  return axios.get(URL, { params: { key: KEY } }).then((response) => response.data.hits);
+export const fetchImages = (page: number): Promise<PixabayImage[]> => {
+  return axios.get(URL, { params: { key: KEY, page } }).then((response) => response.data.hits);
 };
